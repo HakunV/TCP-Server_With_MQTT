b2c72ef79094f7f98f768cff8b8a626ceb150a99
HakunV <hakun_vestergaard@hotmail.com>
2024-05-13 12:20:41 +0200

    Compiled

--- src/main/java/tcpserver/Main.java
+++ src/main/java/tcpserver/Main.java
@@ -8,17 +8,17 @@ import tcpserver.Backend.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        // Server server = new Server();
+        Server server = new Server();
 
-        // server.runServer();
+        server.runServer();
 
-        BackendClient backendClient = new BackendClient();
+        // BackendClient backendClient = new BackendClient();
 
         // backendClient.publish("355688700322392", (float) 30.2324, (float) 13.2390);
 
-        Thread th = new Thread(backendClient);
+        // Thread th = new Thread(backendClient);
 
-        th.start();
+        // th.start();
         // backendClient.publish("355688700322392", (float) 30.2324, (float) 13.2390);
         // backendClient.publish("353994711603560", (float) 47.5432, (float) 9.2342);
     }
