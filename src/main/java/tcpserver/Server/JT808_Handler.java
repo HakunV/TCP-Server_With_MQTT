e09e881b9fe75116763d73e2d4fc39f628d3c52d
HakunV <hakun_vestergaard@hotmail.com>
2024-06-05 00:57:08 +0200

    JT_handler

---
+++ src/main/java/tcpserver/Server/JT808_Handler.java
@@ -0,0 +1,263 @@
+package tcpserver.Server;
+
+import tcpserver.Helpers.Helpers;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class JT808_Handler {
+    ProtocolHandler ph;
+
+    private int byteSize = 2;
+
+    public JT808_Handler(ProtocolHandler ph) {
+        this.ph = ph;
+    }
+
+    public void handleProtocol(String dataString) {
+        String messageId = dataString.substring(2, 6);
+        String msgProps = dataString.substring(6, 10);
+        String phoneNumber = dataString.substring(10, 22);
+        String messageSequence = dataString.substring(22, 26);
+        
+        System.out.println("Message ID: " + messageId);
+        System.out.println("Message Props: " + msgProps);
+        System.out.println("Phone Number: " + phoneNumber);
+        System.out.println("Message Sequence: " + messageSequence);
+        System.out.println();
+
+        String mesBody = dataString.substring(26, dataString.length()-4);
+
+        switch (messageId) {
+            case "0100":
+                System.out.println("Registration Message");
+                System.out.println();
+                handleReg(mesBody, phoneNumber, messageSequence);
+                break;
+            case "0102":
+                System.out.println("Authentication Message");
+                System.out.println();
+                handleAuth(dataString, phoneNumber, messageSequence);
+                break;
+            case "0200":
+                System.out.println("Location Message");
+                System.out.println();
+                handleLoc(dataString);
+                break;
+            case "0704":
+                System.out.println("Data Batch Message");
+                System.out.println();
+                handleBatch(dataString);
+                break;
+            case "0002":
+                System.out.println("Heartbeat Message");
+                System.out.println();
+                break;
+            default:
+                System.out.println("Message ID Not Known");
+                System.out.println();
+                break;
+        }
+    }
+
+    private void handleBatch(String dataString) {
+        int nLocItem = Integer.parseInt(dataString.substring(0, 4), 16);
+        int type = Integer.parseInt(dataString.substring(4, 6), 16);
+
+        int pointer = 6;
+        for (int i = 0; i < nLocItem; i++) {
+            int len = Integer.parseInt(dataString.substring(pointer, pointer+4), 16);
+            pointer = pointer+4;
+
+            String locData = dataString.substring(pointer, pointer+len*byteSize);
+            handleLoc(locData);
+
+            pointer = pointer+len*byteSize;
+        }
+    }
+
+    private void handleLoc(String dataString) {
+        float lat = getGPS(dataString.substring(16, 24));
+
+        System.out.println("Latitude: " + lat);
+        System.out.println();
+
+        float lon = getGPS(dataString.substring(24, 32));
+
+        System.out.println("Longitude: " + lon);
+        System.out.println();
+
+        ph.publish(lat, lon);
+
+        String time = dataString.substring(44, 56);
+        System.out.println("Date: " + time);
+    }
+
+    public float getGPS(String lat) {
+        int l = Integer.parseInt(lat, 16);
+
+        float lf = (float) (l/Math.pow(10, 6));
+
+        return lf;
+    }
+
+    private void handleAuth(String dataString, String phoneNumber, String messageSequence) {
+        String auth = dataString;
+        System.out.println("Authentication Code: " + auth);
+        System.out.println();
+
+        String res = "";
+
+        if (authRegistered(auth)) {
+            ph.setName(getImeiByAuth(auth));
+            ph.checkDups();
+            res = "00";
+        }
+        else {
+            res = "00";
+        }
+
+        String hexString = "80010005" + phoneNumber + "2f82" + messageSequence + "0102" + res;
+
+        byte[] data2 = Helpers.hexStrToByteArr(hexString);
+        String checksum = String.format("%02X", Helpers.calculateChecksum(data2));
+        System.out.println("XOR Checksum: " + checksum + "\n");
+
+        String response = "7e80010005" + phoneNumber + "2f82" + messageSequence + "0102" + res + checksum + "7e";
+
+        try {
+            ph.sendMessage(response);
+        } catch (IOException e) {
+            System.out.println("Could Not Send JT808");
+            e.printStackTrace();
+        }
+
+        System.out.println("Sent registration response: " + response);
+    }
+
+    public String getImeiByAuth(String a) {
+        BufferedReader reader;
+        String res = "";
+
+		try {
+            // For Local Computer
+			// reader = new BufferedReader(new FileReader("C:\\Users\\mariu\\Development\\Bachelor\\Developing\\TCP-Server_With_MQTT\\src\\main\\java\\tcpserver\\Server\\jtTrackers.txt"));
+
+            // For VM
+            reader = new BufferedReader(new FileReader("/home/student/TCP-Server_With_MQTT/src/main/java/tcpserver/Server/jtTrackers.txt"));
+
+			String line = reader.readLine();
+
+			while (line != null) {
+				String[] pair = line.split(",");
+
+                if (a.equals(pair[0])) {
+                    System.out.println("Auth Exists");
+                    System.out.println();
+
+                    res = pair[1];
+                    break;
+                }
+
+				// read next line
+				line = reader.readLine();
+			}
+
+			reader.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+        return res;
+    }
+
+    private void handleReg(String dataString, String phoneNumber, String messageSequence) {
+        String imei = Helpers.hexToString(getManID(dataString)) + phoneNumber.substring(2);
+
+        String auth = Helpers.textToHex(phoneNumber);
+
+        String res = register(auth, imei);
+
+        String hexString = "8100000f" + phoneNumber +"1a61"+ messageSequence +res+auth;
+    
+        byte[] data2 = Helpers.hexStrToByteArr(hexString);
+        String checksum = String.format("%02X", Helpers.calculateChecksum(data2));
+        System.out.println("XOR Checksum: " + checksum + "\n");
+        String response = "7e8100000f" + phoneNumber +"1a61"+ messageSequence +res+auth+checksum+"7e";
+        
+        try {
+            ph.sendMessage(response);
+        } catch (IOException e) {
+            System.out.println("Could Not Send JT808");
+            e.printStackTrace();
+        }
+        System.out.println("Sent registration response: " + response);
+    }
+
+    public String register(String auth, String imei) {
+        
+        if (authRegistered(auth)) {
+            return "03";
+        }
+
+        try(FileWriter fw = new FileWriter("jtTrackers.txt", true);
+            BufferedWriter bw = new BufferedWriter(fw);
+            PrintWriter out = new PrintWriter(bw))
+        {
+            out.println(auth + "," + imei);
+
+            out.close();
+            bw.close();
+            fw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        ph.setName(imei);
+        ph.checkDups();
+
+        return "00";
+    }
+
+    public boolean authRegistered(String auth) {
+        BufferedReader reader;
+        boolean result = false;
+
+		try {
+			// For Local Computer
+			// reader = new BufferedReader(new FileReader("C:\\Users\\mariu\\Development\\Bachelor\\Developing\\TCP-Server_With_MQTT\\src\\main\\java\\tcpserver\\Server\\jtTrackers.txt"));
+
+            // For VM
+            reader = new BufferedReader(new FileReader("/home/student/TCP-Server_With_MQTT/src/main/java/tcpserver/Server/jtTrackers.txt"));
+			String line = reader.readLine();
+
+			while (line != null) {
+				String[] pair = line.split(",");
+
+                if (auth.equals(pair[0])) {
+                    System.out.println("Auth Exists");
+                    System.out.println();
+
+                    result = true;
+                    break;
+                }
+
+				// read next line
+				line = reader.readLine();
+			}
+
+			reader.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+        return result;
+    }
+
+    public String getManID(String str) {
+        return str.substring(8, 18);
+    }
+}
--- src/main/java/tcpserver/Server/ProtocolHandler.java
+++ src/main/java/tcpserver/Server/ProtocolHandler.java
@@ -1,24 +1,20 @@
 package tcpserver.Server;
 
 import tcpserver.Helpers.GT06;
-import tcpserver.Helpers.Helpers;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.io.PrintWriter;
 
 public class ProtocolHandler {
     ClientHandler client;
     private GT06_Handler gh;
+    private JT808_Handler jh;
 
     private int byteSize = 2;
 
     public ProtocolHandler(ClientHandler client) {
         this.client = client;
         this.gh = new GT06_Handler(this);
+        this.jh = new JT808_Handler(this);
     }
 
     public void handleMessage(String dataString) {
@@ -33,207 +29,12 @@ public class ProtocolHandler {
             case "JT808":
                 System.out.println("JT808 Message");
                 System.out.println();
-                handleJT808Message(dataString);
+                jh.handleProtocol(dataString);
                 break;
             default:
                 System.out.println("Unknown protocol");
                 break;
         }
-    }
-
-    private void handleJT808Message(String dataString) {
-        String messageId = dataString.substring(2, 6);
-        String msgProps = dataString.substring(6, 10);
-        String phoneNumber = dataString.substring(10, 22);
-        String messageSequence = dataString.substring(22, 26);
-        
-        System.out.println("Message ID: " + messageId);
-        System.out.println("Message Props: " + msgProps);
-        System.out.println("Phone Number: " + phoneNumber);
-        System.out.println("Message Sequence: " + messageSequence);
-        System.out.println();
-
-        if (messageId.equals("0100")) {
-            String imei = getManID(dataString) + phoneNumber.substring(2);
-
-            String auth = Helpers.textToHex(phoneNumber);
-
-            String res = handleReg(auth, imei);
-
-            String hexString = "8100000f" + phoneNumber +"1a61"+ messageSequence +res+auth;
-        
-            byte[] data2 = Helpers.hexStrToByteArr(hexString);
-            String checksum = String.format("%02X", Helpers.calculateChecksum(data2));
-            System.out.println("XOR Checksum: " + checksum + "\n");
-            String response = "7e8100000f" + phoneNumber +"1a61"+ messageSequence +res+auth+checksum+"7e";
-            
-            try {
-                client.sendMessage(response);
-            } catch (IOException e) {
-                System.out.println("Could Not Send JT808");
-                e.printStackTrace();
-            }
-            System.out.println("Sent registration response: " + response);
-        }
-        else if (messageId.equals("0102")) {
-            String auth = dataString.substring(26, 50);
-            System.out.println("Authentication Code: " + auth);
-            System.out.println();
-
-            String res = "";
-
-            if (authRegistered(auth)) {
-                setName(getImeiByAuth(auth));
-                checkDups();
-                res = "00";
-            }
-            else {
-                res = "00";
-            }
-
-            String hexString = "80010005" + phoneNumber + "2f82" + messageSequence + "0102" + res;
-
-            byte[] data2 = Helpers.hexStrToByteArr(hexString);
-            String checksum = String.format("%02X", Helpers.calculateChecksum(data2));
-            System.out.println("XOR Checksum: " + checksum + "\n");
-
-            String response = "7e80010005" + phoneNumber + "2f82" + messageSequence + "0102" + res + checksum + "7e";
-
-            try {
-                client.sendMessage(response);
-            } catch (IOException e) {
-                System.out.println("Could Not Send JT808");
-                e.printStackTrace();
-            }
-
-            System.out.println("Sent registration response: " + response);
-        }
-        else if (messageId.equals("0200")) {
-            float lat = getGPS(dataString.substring(42, 50));
-
-            System.out.println("Latitude: " + lat);
-            System.out.println();
-
-            float lon = getGPS(dataString.substring(50, 58));
-
-            System.out.println("Longitude: " + lon);
-            System.out.println();
-
-            publish(lat, lon);
-
-            String time = dataString.substring(70, 82);
-            System.out.println("Date: " + time);
-        }
-        else if (messageId.equals("0002")) {
-
-        }
-    }
-
-    public String getManID(String str) {
-        return str.substring(34, 44);
-    }
-
-    public float getGPS(String lat) {
-        int l = Integer.parseInt(lat, 16);
-
-        float lf = (float) (l/Math.pow(10, 6));
-
-        return lf;
-    }
-
-    public String handleReg(String auth, String imei) {
-        
-        if (authRegistered(auth)) {
-            return "03";
-        }
-
-        try(FileWriter fw = new FileWriter("jtTrackers.txt", true);
-            BufferedWriter bw = new BufferedWriter(fw);
-            PrintWriter out = new PrintWriter(bw))
-        {
-            out.println(auth + "," + imei);
-
-            out.close();
-            bw.close();
-            fw.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        setName(imei);
-        checkDups();
-
-        return "00";
-    }
-
-    public boolean authRegistered(String auth) {
-        BufferedReader reader;
-        boolean result = false;
-
-		try {
-			// For Local Computer
-			// reader = new BufferedReader(new FileReader("C:\\Users\\mariu\\Development\\Bachelor\\Developing\\TCP-Server_With_MQTT\\src\\main\\java\\tcpserver\\Server\\jtTrackers.txt"));
-
-            // For VM
-            reader = new BufferedReader(new FileReader("/home/student/TCP-Server_With_MQTT/src/main/java/tcpserver/Server/jtTrackers.txt"));
-			String line = reader.readLine();
-
-			while (line != null) {
-				String[] pair = line.split(",");
-
-                if (auth.equals(pair[0])) {
-                    System.out.println("Auth Exists");
-                    System.out.println();
-
-                    result = true;
-                    break;
-                }
-
-				// read next line
-				line = reader.readLine();
-			}
-
-			reader.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
-        return result;
-    }
-
-    public String getImeiByAuth(String a) {
-        BufferedReader reader;
-        String res = "";
-
-		try {
-            // For Local Computer
-			// reader = new BufferedReader(new FileReader("C:\\Users\\mariu\\Development\\Bachelor\\Developing\\TCP-Server_With_MQTT\\src\\main\\java\\tcpserver\\Server\\jtTrackers.txt"));
-
-            // For VM
-            reader = new BufferedReader(new FileReader("/home/student/TCP-Server_With_MQTT/src/main/java/tcpserver/Server/jtTrackers.txt"));
-
-			String line = reader.readLine();
-
-			while (line != null) {
-				String[] pair = line.split(",");
-
-                if (a.equals(pair[0])) {
-                    System.out.println("Auth Exists");
-                    System.out.println();
-
-                    res = pair[1];
-                    break;
-                }
-
-				// read next line
-				line = reader.readLine();
-			}
-
-			reader.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
-        return res;
     }
 
     private void handleGT06Message(String dataString) {
@@ -270,13 +71,14 @@ public class ProtocolHandler {
             return "GT06";
         }
 
-        if (dataString.startsWith("7979")) {
+        else if (dataString.startsWith("7979")) {
             return "GT06";
         }
         // JT808 message starts with specified message type identifier
         else if (dataString.startsWith("7e")) {
             return "JT808";
-        } else {
+        } 
+        else {
             return "Unknown";
         }
     }
@@ -311,5 +113,9 @@ public class ProtocolHandler {
 
     public void respondToAlarm(String r) throws IOException {
         client.respondToAlarm(r);
+    }
+
+    public void sendMessage(String mes) throws IOException {
+        client.sendMessage(mes);
     }
 }
